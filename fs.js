9538f3ade2eccca660ac00914bd4ea813a31e89f
Sebastian <133081239+Sebas2203@users.noreply.github.com>
2025-03-07 12:10:27 -0600

    fs writeFileSync

--- fs.js
+++ fs.js
@@ -2,4 +2,15 @@ const fs = require("fs");
 
 //leer un archivo
 const first = fs.readFileSync("./data/first.txt", "utf-8");
+const second = fs.readFileSync("./data/second.txt");
+
 console.log(first);
+console.log(second.toString());
+
+//crear un archivo
+fs.writeFileSync("./data/third.txt", "Hello from Node.js");
+
+const title = "Node.js 4 archive";
+fs.writeFileSync("./data/fourth.txt", title, {
+  flag: "a", //append
+});
