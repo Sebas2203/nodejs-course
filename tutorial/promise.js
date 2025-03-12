1abcf9979b4a34adbd10abd1705c259e6a8beb37
Sebastian Mora <133081239+Sebas2203@users.noreply.github.com>
2025-03-11 23:23:17 -0600

    fs/promises

--- tutorial/promise.js
+++ tutorial/promise.js
@@ -1,12 +1,9 @@
-const { readFile } = require("fs");
-const { promisify } = require("util");
-
-const readFilePromie = promisify(readFile);
+const { readFile } = require("fs/promises");
 
 async function read() {
   try {
-    const result = await readFilePromie("./data/first.txt", "utf-8");
-    const result2 = await readFilePromie("./data/second.txt", "utf-8");
+    const result = await readFile("./data/first.txt", "utf-8");
+    const result2 = await readFile("./data/second.txt", "utf-8");
     console.log(result);
     console.log(result2);
   } catch (error) {
@@ -15,21 +12,3 @@ async function read() {
 }
 
 read();
-
-//instanciar el objeto de promise
-// const getText = (pathFile) => {
-//   return new Promise(function (resolve, reject) {
-//     readFile(pathFile, "utf-8", (err, data) => {
-//       if (err) {
-//         reject(err);
-//       }
-//       resolve(data);
-//     });
-//   });
-// };
-
-// getText("./data/first.txt")
-//   .then((result) => console.log(result))
-//   .then(() => getText("./data/second.txt"))
-//   .then((result) => console.log(result))
-//   .catch((error) => console.log(error));
