1bca0d30a7bacb7bb32e6e35517064876e95b356
Sebastian Mora <133081239+Sebas2203@users.noreply.github.com>
2025-03-13 22:59:07 -0600

    es6

---
+++ tutorial/esmodule.js
@@ -0,0 +1,6 @@
+// import math from "./math/index.js";
+// import { add } from "./math/index.js";
+import math, { add, subtract, multiply, divide } from "./math/index.js";
+
+// console.log(math.add(1, 1));
+console.log(add(1, 1));
--- tutorial/math/index.js
+++ tutorial/math/index.js
@@ -1,14 +1,16 @@
-const add = (x, y) => x + y;
+export const add = (x, y) => x + y;
 
-const subtract = (x, y) => x - y;
+export const subtract = (x, y) => x - y;
 
-const multiply = (x, y) => x * y;
+export const multiply = (x, y) => x * y;
 
-const divide = (x, y) => x / y;
+export const divide = (x, y) => x / y;
 
-module.exports = {
+export default {
   add,
   subtract,
   multiply,
   divide,
 };
+
+//las puedo exportar individualmente o en grupo
